b81b27373fba860fd37e240c121f6cf1b40dc8eb
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-06-24 00:02:52 +0300

    first commit

--- config/db.js
+++ config/db.js
@@ -6,10 +6,7 @@ dotenv.config(); // Load env vars
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      // useNewUrlParser: true, // To avoid deprecation warning
-      // useUnifiedTopology: true, // To avoid deprecation warning
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
